b6ab3ca107d44e9b47166c3daf0c2b9b34f2a832
Petar <parandjic@gmail.com>
2020-03-08 19:33:19 +0100

    Ghost items that calculates position of widgets

---
+++ src/components/pi-dash/GhostItem.vue
@@ -0,0 +1,44 @@
+<template>
+    <div
+    class="ghostItem"
+    @mouseover="getChosenPosition(position)"
+    :style="{
+    gridRow: position.row + '/' + (position.row + 1),
+    gridColumn: position.column + '/' + (position.column + 1)
+    }"
+    ></div>
+</template>
+
+<script>
+    export default {
+        props: {
+            pAtt: Object,
+            getChosenPosition: Function
+        },
+        data(){
+            return {
+                position: null
+            }
+        },
+        created() {
+            this.position = this.pAtt
+        }
+    }
+</script>
+
+<style scoped>
+    .ghostItem{
+
+        -webkit-user-select: none;
+        -khtml-user-select: none;
+        -moz-user-select: none;
+        -o-user-select: none;
+        user-select: none;
+
+        -webkit-user-drag: none;
+        -khtml-user-drag: none;
+        -moz-user-drag: none;
+        -o-user-drag: none;
+        user-drag: none;
+    }
+</style>
